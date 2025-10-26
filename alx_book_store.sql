1ce95fc608197d3b44af450a375ef742fa52a3ee
yrnmalone0 <obedbadu04@gmail.com>
2025-10-26 06:40:50 +0000

    few changes made

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -13,8 +13,7 @@ CREATE TABLE Books(
     author_id INT,
     price DOUBLE,
     publication_date DATE,
-    FOREIGN KEY(author_id) REFERENCES 
-    Authors(author_id)
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
 CREATE TABLE Customers(
@@ -28,8 +27,7 @@ CREATE TABLE Orders(
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY(customer_id) REFERENCES 
-    Customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Details(
@@ -37,8 +35,6 @@ CREATE TABLE Order_Details(
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY(order_id) REFERENCES 
-    Orders(order_id),
-    FOREIGN KEY(book_id) REFERENCES 
-    Books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
