ee118f5644cd481683405da74ba94101e2a0a7a4
yrnmalone0 <obedbadu04@gmail.com>
2025-10-27 04:18:34 +0000

    populated my DB with a data

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,4 @@
-INSERT INTO alx_book_store.customers(customer_id, customer_name, email, address)
+USE alx_book_store;
+
 VALUES(1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.")
+INSERT INTO customer(customer_id, customer_name, email, address)
