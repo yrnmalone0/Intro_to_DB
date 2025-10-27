d956e01855ee5301eb97767682240a046f667bd4
yrnmalone0 <obedbadu04@gmail.com>
2025-10-27 04:27:21 +0000

    inserted multiple rows in the table customer

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,5 @@
 USE alx_book_store;
 
-VALUES(1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.")
+
 INSERT INTO customer(customer_id, customer_name, email, address)
+VALUES(1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.");
